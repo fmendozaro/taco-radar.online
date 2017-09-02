cd2d90deee764360bb18be0c27fb83c3556f7000
Fernando <fernandoza@gmail.com>
2017-09-01 20:21:10 -0500

    Prepare files for deployment.

--- src/main/java/online/tacoradar/app/AppApplication.java
+++ src/main/java/online/tacoradar/app/AppApplication.java
@@ -2,11 +2,17 @@ package online.tacoradar.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AppApplication {
+public class AppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
+	}
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AppApplication.class);
 	}
 }
