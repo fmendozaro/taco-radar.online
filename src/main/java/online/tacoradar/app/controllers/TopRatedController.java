15a93c8e24ee39afdf3664f764e4db34473390b3
Fernando <fernandoza@gmail.com>
2017-11-28 11:22:54 -0600

    Connect the navbar routes with the new Controller

--- src/main/java/online/tacoradar/app/controllers/TopRatedController.java
+++ src/main/java/online/tacoradar/app/controllers/TopRatedController.java
@@ -2,12 +2,13 @@ package online.tacoradar.app.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class TopRatedController {
 
-    @GetMapping("/toprated")
-    public String showTopRated(){
+    @GetMapping("/toprated/{category}")
+    public String showTopRated(@PathVariable String category){
         return "top-rated";
     }
 }
