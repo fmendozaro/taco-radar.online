6e940c42249c8321924f97cd5c857ea5bee70147
fer <fernandoza@gmail.com>
2017-12-13 11:35:51 -0600

    Change to better colors.

--- src/main/resources/static/js/maps.js
+++ src/main/resources/static/js/maps.js
@@ -116,7 +116,7 @@ $(document).ready(function(){
             "<br/>" +
             "<img class='center-align center' src='" + photo + "' /><br/>"+
             "<button id='btn-review' class='btn bg-green-flag'>Add review</button><br/>" +
-            "<button id='btn-fav' class='btn yellow accent-3'>Add to Favs</button>";
+            "<button id='btn-fav' class='btn yellow accent-4'>Add to Favs</button>";
 
         var marker = new google.maps.Marker({
             map: map,
